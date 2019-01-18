e50afb1b20f6b4119f14723cd95ae30885b9ed27
Nicola Piga <nicolapiga@gmail.com>
2019-01-18 11:54:35 +0100

    Add class PointCloudModel

---
+++ src/object-tracking/include/PointCloudModel.h
@@ -0,0 +1,35 @@
+#ifndef POINTCLOUDMODEL_H
+#define POINTCLOUDMODEL_H
+
+#include <BayesFilters/AdditiveMeasurementModel.h>
+
+#include <PointCloudPrediction.h>
+
+#include <Eigen/Dense>
+
+#include <memory>
+
+
+class PointCloudModel : public bfl::AdditiveMeasurementModel
+{
+public:
+    PointCloudModel(std::unique_ptr<PointCloudPrediction> prediction, const Eigen::Ref<const Eigen::Matrix3d>& noise_covariance_matrix);
+
+    std::pair<bool, bfl::Data> predictedMeasure(const Eigen::Ref<const Eigen::MatrixXd>& current_states) const override;
+
+    std::pair<bool, bfl::Data> innovation(const bfl::Data& predicted_measurements, const bfl::Data& measurements) const override;
+
+    std::pair<bool, Eigen::MatrixXd> getNoiseCovarianceMatrix() const override;
+
+protected:
+    std::unique_ptr<PointCloudPrediction> prediction_;
+
+    Eigen::Matrix3d model_noise_covariance_;
+
+    std::vector<std::string> log_filenames(const std::string& prefix_path, const std::string& prefix_name) override
+    {
+        return {prefix_path + "/" + prefix_name + "_measurements"};
+    }
+};
+
+#endif

d76e036c30b7e1e8856284524b00660d55e35ef9
Nicola Piga <nicolapiga@gmail.com>
2019-01-19 15:45:25 +0100

    Method MeshImporter::getMesh is public

--- src/object-tracking/include/MeshImporter.h
+++ src/object-tracking/include/MeshImporter.h
@@ -13,10 +13,10 @@ class MeshImporter
 public:
     MeshImporter(const std::string& mesh_filename);
 
+    std::pair<bool, std::istringstream> getMesh(const std::string& format_id);
+
 protected:
     std::unique_ptr<aiScene> mesh_;
-
-    std::pair<bool, std::istringstream> getMesh(const std::string& format_id);
 };
 
 #endif /* MESHIMPORTER_H */

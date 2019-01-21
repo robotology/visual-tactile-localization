41f0156fc53916dcdb931b41485cae676c1169aa
Nicola Piga <nicolapiga@gmail.com>
2019-01-21 15:10:50 +0100

    Working simulation scenario within src/object-tracking/src/main.cpp

--- src/object-tracking/src/main.cpp
+++ src/object-tracking/src/main.cpp
@@ -6,6 +6,12 @@
 #include <SimulatedFilter.h>
 #include <SimulatedPointCloud.h>
 
+#include <BayesFilters/AdditiveMeasurementModel.h>
+#include <BayesFilters/GaussianCorrection.h>
+#include <BayesFilters/GaussianFilter.h>
+#include <BayesFilters/GaussianPrediction.h>
+#include <BayesFilters/KFPrediction.h>
+
 #include <yarp/os/LogStream.h>
 #include <yarp/os/ResourceFinder.h>
 
@@ -15,8 +21,9 @@
 #include <string>
 #include <sstream>
 
-using namespace yarp::os;
+using namespace bfl;
 using namespace Eigen;
+using namespace yarp::os;
 
 VectorXd loadVectorDouble
 (
@@ -87,41 +94,42 @@ int main(int argc, char** argv)
     rf.configure(argc, argv);
 
     /* Get mode of operation. */
-    ResourceFinder mode_parameters = rf.findNestedResourceFinder("MODE");
-    const std::string mode  = mode_parameters.check("mode",  Value("simulation")).asString();
-    const std::string robot = mode_parameters.check("robot", Value("icubSim")).asString();
+    ResourceFinder rf_mode_parameters = rf.findNestedResourceFinder("MODE");
+    const std::string mode  = rf_mode_parameters.check("mode",  Value("simulation")).asString();
+    const std::string robot = rf_mode_parameters.check("robot", Value("icubSim")).asString();
 
     /* Get initial condition. */
-    ResourceFinder initial_conditions = rf.findNestedResourceFinder("INITIAL_CONDITION");
-    VectorXd x_0           = loadVectorDouble(initial_conditions, "x_0",           3);
-    VectorXd v_0           = loadVectorDouble(initial_conditions, "v_0",           3);
-    VectorXd euler_0       = loadVectorDouble(initial_conditions, "euler_0",       3);
-    VectorXd euler_dot_0   = loadVectorDouble(initial_conditions, "euler_dot_0",   3);
-    VectorXd cov_x_0       = loadVectorDouble(initial_conditions, "cov_x_0",       3);
-    VectorXd cov_v_0       = loadVectorDouble(initial_conditions, "cov_v_0",       3);
-    VectorXd cov_eul_0     = loadVectorDouble(initial_conditions, "cov_eul_0",     3);
-    VectorXd cov_eul_dot_0 = loadVectorDouble(initial_conditions, "cov_eul_dot_0", 3);
+    ResourceFinder rf_initial_conditions = rf.findNestedResourceFinder("INITIAL_CONDITION");
+    VectorXd x_0           = loadVectorDouble(rf_initial_conditions, "x_0",           3);
+    VectorXd v_0           = loadVectorDouble(rf_initial_conditions, "v_0",           3);
+    VectorXd euler_0       = loadVectorDouble(rf_initial_conditions, "euler_0",       3);
+    VectorXd euler_dot_0   = loadVectorDouble(rf_initial_conditions, "euler_dot_0",   3);
+    VectorXd cov_x_0       = loadVectorDouble(rf_initial_conditions, "cov_x_0",       3);
+    VectorXd cov_v_0       = loadVectorDouble(rf_initial_conditions, "cov_v_0",       3);
+    VectorXd cov_eul_0     = loadVectorDouble(rf_initial_conditions, "cov_eul_0",     3);
+    VectorXd cov_eul_dot_0 = loadVectorDouble(rf_initial_conditions, "cov_eul_dot_0", 3);
 
     /* Kinematic model. */
-    ResourceFinder kinematic_model = rf.findNestedResourceFinder("KINEMATIC_MODEL");
-    double sample_time     = kinematic_model.check("sample_time", Value("1.0")).asDouble();
-    VectorXd kin_psd_acc   = loadVectorDouble(kinematic_model, "psd_acc", 3);
-    VectorXd kin_psd_euler = loadVectorDouble(kinematic_model, "psd_euler_acc", 3);
+    ResourceFinder rf_kinematic_model = rf.findNestedResourceFinder("KINEMATIC_MODEL");
+    double sample_time     = rf_kinematic_model.check("sample_time", Value("1.0")).asDouble();
+    VectorXd kin_psd_acc   = loadVectorDouble(rf_kinematic_model, "psd_acc", 3);
+    VectorXd kin_psd_euler = loadVectorDouble(rf_kinematic_model, "psd_euler_acc", 3);
 
     /* Measurement model. */
-    ResourceFinder measurement_model = rf.findNestedResourceFinder("MEASUREMENT_MODEL");
-    VectorXd noise_covariance = loadVectorDouble(measurement_model, "noise_covariance", 3);
+    ResourceFinder rf_measurement_model = rf.findNestedResourceFinder("MEASUREMENT_MODEL");
+    VectorXd noise_covariance = loadVectorDouble(rf_measurement_model, "noise_covariance", 3);
 
     /* Unscented transform. */
-    ResourceFinder unscented_transform = rf.findNestedResourceFinder("UNSCENTED_TRANSFORM");
-    double ut_alpha = unscented_transform.check("alpha", Value("1.0")).asDouble();
-    double ut_beta  = unscented_transform.check("beta", Value("2.0")).asDouble();
-    double ut_kappa = unscented_transform.check("kappa", Value("0.0")).asDouble();
+    ResourceFinder rf_unscented_transform = rf.findNestedResourceFinder("UNSCENTED_TRANSFORM");
+    double ut_alpha = rf_unscented_transform.check("alpha", Value("1.0")).asDouble();
+    double ut_beta  = rf_unscented_transform.check("beta", Value("2.0")).asDouble();
+    double ut_kappa = rf_unscented_transform.check("kappa", Value("0.0")).asDouble();
 
     /* Point cloud prediction. */
-    ResourceFinder point_cloud_prediction = rf.findNestedResourceFinder("POINT_CLOUD_PREDICTION");
-    std::size_t pc_pred_num_samples = point_cloud_prediction.check("number_samples", Value("100")).asInt();
+    ResourceFinder rf_point_cloud_prediction = rf.findNestedResourceFinder("POINT_CLOUD_PREDICTION");
+    std::size_t pc_pred_num_samples = rf_point_cloud_prediction.check("number_samples", Value("100")).asInt();
 
+    /* Simulation parameters. */
     double sim_sample_time;
     double sim_duration;
     VectorXd sim_psd_acc;
@@ -137,21 +145,36 @@ int main(int argc, char** argv)
     bool sim_point_cloud_back_culling;
     if (mode == "simulation")
     {
-        ResourceFinder simulation = rf.findNestedResourceFinder("SIMULATION");
-        sim_sample_time = simulation.check("sample_time", Value("1.0")).asDouble();
-        sim_duration    = simulation.check("duration", Value("1.0")).asDouble();
-        sim_psd_acc     = loadVectorDouble(simulation, "psd_acc", 3);
-        sim_psd_w       = loadVectorDouble(simulation, "psd_w", 3);
-        sim_x_0         = loadVectorDouble(simulation, "x_0", 3);
-        sim_v_0         = loadVectorDouble(simulation, "v_0", 3);
-        sim_q_0         = loadVectorDouble(simulation, "q_0", 4);
-        sim_w_0         = loadVectorDouble(simulation, "w_0", 3);
+        ResourceFinder rf_simulation = rf.findNestedResourceFinder("SIMULATION");
+        sim_sample_time = rf_simulation.check("sample_time", Value("1.0")).asDouble();
+        sim_duration    = rf_simulation.check("duration", Value("1.0")).asDouble();
+        sim_psd_acc     = loadVectorDouble(rf_simulation, "psd_acc", 3);
+        sim_psd_w       = loadVectorDouble(rf_simulation, "psd_w", 3);
+        sim_x_0         = loadVectorDouble(rf_simulation, "x_0", 3);
+        sim_v_0         = loadVectorDouble(rf_simulation, "v_0", 3);
+        sim_q_0         = loadVectorDouble(rf_simulation, "q_0", 4);
+        sim_w_0         = loadVectorDouble(rf_simulation, "w_0", 3);
 
-        ResourceFinder sim_point_cloud = rf.findNestedResourceFinder("SIMULATED_POINT_CLOUD");
-        sim_point_cloud_number       = sim_point_cloud.check("number_points", Value("1000")).asInt();
-        sim_point_cloud_observer     = loadVectorDouble(sim_point_cloud, "observer_origin", 3);
-        sim_point_cloud_noise_std    = loadVectorDouble(sim_point_cloud, "noise_std", 3);
-        sim_point_cloud_back_culling = sim_point_cloud.check("back_cullnig", Value(true)).asBool();
+        ResourceFinder rf_sim_point_cloud = rf.findNestedResourceFinder("SIMULATED_POINT_CLOUD");
+        sim_point_cloud_number       = rf_sim_point_cloud.check("number_points", Value("1000")).asInt();
+        sim_point_cloud_observer     = loadVectorDouble(rf_sim_point_cloud, "observer_origin", 3);
+        sim_point_cloud_noise_std    = loadVectorDouble(rf_sim_point_cloud, "noise_std", 3);
+        sim_point_cloud_back_culling = rf_sim_point_cloud.check("back_cullnig", Value(true)).asBool();
+    }
+
+    /* Mesh parameters. */
+    ResourceFinder rf_object = rf.findNestedResourceFinder("OBJECT");
+    const std::string object_name = rf_object.check("object_name", Value("ycb_mustard")).asString();
+    const std::string object_mesh_path = rf.findPath("mesh/" + object_name) + "/nontextured.ply";
+
+    /* Logging parameters. */
+    ResourceFinder rf_logging = rf.findNestedResourceFinder("LOG");
+    bool enable_log = rf_logging.check("enable_log", Value(false)).asBool();
+    const std::string log_path = rf_logging.check("absolute_log_path", Value("")).asString();
+    if (enable_log && log_path == "")
+    {
+        yWarning() << "Invalid log path. Disabling log...";
+        enable_log = false;
     }
 
     /* Log parameters. */
@@ -204,5 +227,188 @@ int main(int argc, char** argv)
         yInfo() << log_ID << "- back_culling:"    << sim_point_cloud_back_culling;
     }
 
-    return 0;
+    yInfo() << log_ID << "Object:";
+    yInfo() << log_ID << "- object_name:"  << object_name;
+    yInfo() << log_ID << "- mesh path is:" << object_mesh_path;
+
+    yInfo() << log_ID << "Logging:";
+    yInfo() << log_ID << "- enable_log:"        << enable_log;
+    yInfo() << log_ID << "- absolute_log_path:" << log_path;
+
+    /**
+     * Prepare pointer to the measurement model.
+     */
+    std::unique_ptr<AdditiveMeasurementModel> measurement_model;
+
+    /**
+     * Initialize point cloud prediction.
+     */
+    std::unique_ptr<PointCloudPrediction> pc_prediction =
+        std::unique_ptr<NanoflannPointCloudPrediction>(new NanoflannPointCloudPrediction(object_mesh_path, pc_pred_num_samples));
+
+
+    /**
+     * Initialize simulation if required.
+     */
+    if (mode == "simulation")
+    {
+        /**
+         * Initial condition of simulation.
+         */
+        VectorXd state_0(3 + 3 + 4 + 3);
+        state_0.head(3) = sim_x_0;
+        state_0.segment(3, 3) = sim_v_0;
+        state_0.segment(6, 4) = sim_q_0;
+        state_0.tail(3) = sim_w_0;
+
+        /**
+         * Initialize random pose generator.
+         */
+        std::unique_ptr<StateModel> rand_pose(
+            new Random3DPose(sim_sample_time,
+                             sim_psd_acc(0), sim_psd_acc(1), sim_psd_acc(2),
+                             sim_psd_w(0), sim_psd_w(1), sim_psd_w(2)));
+
+        /**
+         * Initialize simulated state model.
+         */
+        std::unique_ptr<SimulatedStateModel> sim_rand_pose(
+            new SimulatedStateModel(std::move(rand_pose), state_0, sim_duration / sim_sample_time));
+        if (enable_log)
+            sim_rand_pose->enable_log(log_path, "object-tracking");
+
+        /**
+         * Initialize simulated measurement model.
+         */
+        MatrixXd noise_covariance_diagonal = noise_covariance.asDiagonal();
+        std::unique_ptr<SimulatedPointCloud> pc_simulation =
+            std::unique_ptr<SimulatedPointCloud>(new SimulatedPointCloud(object_mesh_path,
+                                                                         std::move(pc_prediction),
+                                                                         std::move(sim_rand_pose),
+                                                                         // This is the modeled noise covariance
+                                                                         noise_covariance_diagonal,
+                                                                         sim_point_cloud_observer,
+                                                                         sim_point_cloud_number,
+                                                                         sim_point_cloud_back_culling));
+        // This the actual simulated noise standard deviation
+        pc_simulation->enableNoise(sim_point_cloud_noise_std(0),
+                                   sim_point_cloud_noise_std(1),
+                                   sim_point_cloud_noise_std(2));
+
+        // Prefetch measurements
+        std::cout << "Prefetching measurements for faster simulation..." << std::flush;
+        pc_simulation->prefetchMeasurements(sim_duration / sim_sample_time);
+        std::cout << "done." << std::endl;
+
+        measurement_model = std::move(pc_simulation);
+    }
+
+    if (enable_log)
+        measurement_model->enable_log(log_path, "object-tracking");
+
+    /**
+     * Filter construction.
+     */
+
+    /**
+     * StateModel
+     */
+    std::unique_ptr<LinearStateModel> kinematic_model = std::unique_ptr<KinematicModel>(
+        new KinematicModel(sample_time,
+                           kin_psd_acc(0), kin_psd_acc(1), kin_psd_acc(2),
+                           kin_psd_euler(0), kin_psd_euler(1), kin_psd_euler(2)));
+
+    std::size_t dim_linear;
+    std::size_t dim_circular;
+    std::tie(dim_linear, dim_circular) = kinematic_model->getOutputSize();
+    std::size_t state_size = dim_linear + dim_circular;
+
+    /**
+     * Initial condition.
+     */
+    Gaussian initial_state(dim_linear, dim_circular);
+    VectorXd initial_mean(12);
+    initial_mean.head<3>() = x_0;
+    initial_mean.segment<3>(3) = v_0;
+    initial_mean.segment<3>(6) = euler_dot_0;
+    initial_mean.tail<3>() = euler_0;
+
+    VectorXd initial_covariance(12);
+    initial_covariance.head<3>() = cov_x_0;
+    initial_covariance.segment<3>(3) = cov_v_0;
+    initial_covariance.segment<3>(6) = cov_eul_dot_0;
+    initial_covariance.tail<3>() = cov_eul_0;
+
+    initial_state.mean() =  initial_mean;
+    initial_state.covariance() = initial_covariance.asDiagonal();
+
+    /**
+     * Prediction step.
+     */
+    std::unique_ptr<GaussianPrediction> prediction =
+        std::unique_ptr<KFPrediction>(new KFPrediction(std::move(kinematic_model)));
+
+    /**
+     * Correction step.
+     */
+    std::size_t measurement_sub_size = 3; // i.e. a measurement is made of 3 * N points
+                                          // where N is the number of points belonging to the point cloud
+
+    std::unique_ptr<GaussianCorrection> correction =
+        std::unique_ptr<Correction>(new Correction(std::move(measurement_model),
+                                                   state_size,
+                                                   ut_alpha, ut_beta, ut_kappa,
+                                                   measurement_sub_size));
+    /**
+     * Filter.
+     */
+    std::cout << "Initializing filter..." << std::flush;
+
+    std::unique_ptr<GaussianFilter> filter;
+    if (mode == "simulation")
+    {
+        filter = std::move(std::unique_ptr<SimulatedFilter>(
+                               new SimulatedFilter(initial_state,
+                                                   std::move(prediction),
+                                                   std::move(correction),
+                                                   sim_duration / sim_sample_time)));
+    }
+
+    std::cout << "done." << std::endl;
+
+    if (enable_log)
+        filter->enable_log(log_path, "object-tracking");
+
+    std::cout << "Booting filter..." << std::flush;
+
+    filter->boot();
+
+    std::cout << "done." << std::endl;
+
+    if (mode == "simulation")
+    {
+        std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+        filter->run();
+
+        std::cout << "Running filter..." << std::endl;
+
+        if (!filter->wait())
+            return EXIT_FAILURE;
+
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+        std::cout << "Done in "
+                  << std::chrono::duration_cast<std::chrono::seconds>(end - start).count()
+                  << "s"
+                  << std::endl;
+    }
+    else
+    {
+        std::cout << "Running filter..." << std::endl;
+
+        if (!filter->wait())
+            return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }

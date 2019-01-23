c5e16dd6dd5ce207f5a6e2aa9220796366a54f2f
Nicola Piga <nicolapiga@gmail.com>
2019-01-23 21:30:24 +0100

    Re-implement GazeController in order to use torse and head encoders in case the gaze interface is not available

--- src/gaze-ctrl-library/include/GazeController.h
+++ src/gaze-ctrl-library/include/GazeController.h
@@ -1,17 +1,22 @@
 #ifndef GAZE_CONTROLLER_H
 #define GAZE_CONTROLLER_H
 
+#include <iCub/iKin/iKinFwd.h>
+
 #include <yarp/dev/GazeControl.h>
 #include <yarp/dev/PolyDriver.h>
+#include <yarp/os/Bottle.h>
+#include <yarp/os/BufferedPort.h>
+
 
 class GazeController
 {
 public:
-    GazeController(const std::string &port_prefix);
+    GazeController(const std::string port_prefix);
 
     virtual ~GazeController();
 
-    bool getCameraPose(const std::string eye_name, yarp::sig::Vector &pos, yarp::sig::Vector &att);
+    bool getCameraPoses(yarp::sig::Vector& pos_left, yarp::sig::Vector& att_left, yarp::sig::Vector& pos_right, yarp::sig::Vector& att_right);
 
     bool getCameraIntrinsics(const std::string eye_name, double &fx, double &fy, double &cx, double &cy);
 
@@ -19,6 +24,30 @@ private:
     yarp::dev::PolyDriver drv_gaze;
 
     yarp::dev::IGazeControl *igaze;
+
+    yarp::os::BufferedPort<yarp::os::Bottle> port_head_enc_;
+
+    yarp::os::BufferedPort<yarp::os::Bottle> port_torso_enc_;
+
+    iCub::iKin::iCubEye icub_kin_eye_left_;
+
+    iCub::iKin::iCubEye icub_kin_eye_right_;
+
+    double fx_left_;
+
+    double fy_left_;
+
+    double cx_left_;
+
+    double cy_left_;
+
+    double fx_right_;
+
+    double fy_right_;
+
+    double cx_right_;
+
+    double cy_right_;
 };
 
 #endif /* GAZE_CONTROLLER_H */
--- src/gaze-ctrl-library/src/GazeController.cpp
+++ src/gaze-ctrl-library/src/GazeController.cpp
@@ -2,78 +2,153 @@
 
 #include <yarp/os/LogStream.h>
 #include <yarp/os/Bottle.h>
+#include <yarp/os/ResourceFinder.h>
 #include <yarp/math/Math.h>
 
+#include <cmath>
+
 using namespace yarp::os;
+using namespace yarp::sig;
+using namespace iCub::iKin;
 
-
-GazeController::GazeController(const std::string &port_prefix)
+GazeController::GazeController(const std::string port_prefix)
 {
     /**
      * Drivers configuration
      */
 
-    // prepare properties for the GazeController
+    // Prepare properties for the GazeController
     Property prop;
     prop.put("device", "gazecontrollerclient");
     prop.put("remote", "/iKinGazeCtrl");
     prop.put("local", port_prefix + "/gazecontroller");
 
-    // let's give the controller some time to warm up
-    bool driver_ok = false;
-    double t0 = SystemClock::nowSystem();
-    while (SystemClock::nowSystem() - t0 < 10.0)
+    if (!(drv_gaze.open(prop) && drv_gaze.view(igaze) && igaze != nullptr))
     {
-        // this might fail if controller
-        // is not connected to solver yet
-        if (drv_gaze.open(prop))
-        {
-            driver_ok= true;
-            break;
-        }
-        SystemClock::delaySystem(1.0);
-    }
-    if (!driver_ok)
-    {
-        std::string err = "GAZECONTROLLER::CTOR::ERROR\n\tError: cannot open the Gaze controller driver.";
-        throw(std::runtime_error(err));
-    }
+        yWarning() << "GAZECONTROLLER::CTOR. Warning: cannot open the Gaze controller driver, switching to the encoders.";
 
-    // try to retrieve the view
-    driver_ok = drv_gaze.view(igaze);
-    if (!driver_ok || igaze == nullptr)
-    {
-        std::string err = "GAZECONTROLLER::CTOR::ERROR\n\tError: cannot retrieve the Gaze controller driver.";
-        throw(std::runtime_error(err));
+        // Open ports
+        port_head_enc_.open("/" + port_prefix + "/icub/head:i");
+        port_torso_enc_.open("/" + port_prefix + "/icub/torso:i");
+
+        // Initialize forward kinematics
+        icub_kin_eye_left_ = iCubEye("left_v2");
+        icub_kin_eye_left_.setAllConstraints(false);
+        icub_kin_eye_left_.releaseLink(0);
+        icub_kin_eye_left_.releaseLink(1);
+        icub_kin_eye_left_.releaseLink(2);
+
+        icub_kin_eye_right_ = iCubEye("right_v2");
+        icub_kin_eye_right_.setAllConstraints(false);
+        icub_kin_eye_right_.releaseLink(0);
+        icub_kin_eye_right_.releaseLink(1);
+        icub_kin_eye_right_.releaseLink(2);
+
+        // Also load instrinsic parameters from configuration file
+        // installed by the package 'object-tracking'
+        ResourceFinder rf;
+        rf.setVerbose();
+        rf.setDefaultContext("object-tracking");
+        rf.setDefaultConfigFile("sfm_config.ini");
+        rf.configure(0, nullptr);
+
+        ResourceFinder rf_intrinsics_left = rf.findNestedResourceFinder("CAMERA_CALIBRATION_LEFT");
+        fx_left_ = rf_intrinsics_left.check("fx", Value(234.666)).asDouble();
+        fy_left_ = rf_intrinsics_left.check("fy", Value(234.25)).asDouble();
+        cx_left_ = rf_intrinsics_left.check("cx", Value(149.795)).asDouble();
+        cy_left_ = rf_intrinsics_left.check("cy", Value(123.059)).asDouble();
+
+        ResourceFinder rf_intrinsics_right = rf.findNestedResourceFinder("CAMERA_CALIBRATION_RIGHT");
+        fx_right_ = rf_intrinsics_right.check("fx", Value(234.88)).asDouble();
+        fy_right_ = rf_intrinsics_right.check("fy", Value(234.582)).asDouble();
+        cx_right_ = rf_intrinsics_right.check("cx", Value(160.77)).asDouble();
+        cy_right_ = rf_intrinsics_right.check("cy", Value(123.491)).asDouble();
     }
 }
 
 
 GazeController::~GazeController()
 {
-    // close the driver
-    drv_gaze.close();
+    if (igaze != nullptr)
+    {
+        // Close the driver
+        drv_gaze.close();
+    }
+    else
+    {
+        // Close the ports
+        port_head_enc_.close();
+        port_torso_enc_.close();
+    }
 }
 
 
-bool GazeController::getCameraPose
+bool GazeController::getCameraPoses
 (
-    const std::string eye_name,
-    yarp::sig::Vector &pos,
-    yarp::sig::Vector &att
+    yarp::sig::Vector& pos_left,
+    yarp::sig::Vector& att_left,
+    yarp::sig::Vector& pos_right,
+    yarp::sig::Vector& att_right
 )
 {
-    if ((eye_name != "right") && (eye_name != "left"))
-        return false;
+    if (igaze != nullptr)
+    {
+        return igaze->getLeftEyePose(pos_left, att_left) &&
+               igaze->getRightEyePose(pos_right, att_right);
+    }
+    else
+    {
+        Vector root_eye_enc(8, 0.0);
 
-    bool valid_pose = false;
+        Bottle* bottle_torso = port_torso_enc_.read(true);
+        Bottle* bottle_head  = port_head_enc_.read(true);
+        if (!bottle_torso || !bottle_head)
+            return false;
 
-    if (eye_name == "right")
-        valid_pose = igaze->getRightEyePose(pos, att);
-    else if(eye_name == "left")
-        valid_pose = igaze->getLeftEyePose(pos, att);
+        // Torso in reversed order
+        root_eye_enc(0) = bottle_torso->get(2).asDouble();
+        root_eye_enc(1) = bottle_torso->get(1).asDouble();
+        root_eye_enc(2) = bottle_torso->get(0).asDouble();
 
-    return valid_pose;
+        // Neck and eyes tilt
+        for (size_t i = 0; i < 4; ++i)
+            root_eye_enc(3 + i) = bottle_head->get(i).asDouble();
+
+        // Left eye dof from version and vergence
+        root_eye_enc(7) = bottle_head->get(4).asDouble() + bottle_head->get(5).asDouble() / 2.0;
+
+        // Extract pose
+        Vector left_eye_pose = icub_kin_eye_left_.EndEffPose(M_PI / 180.0 * root_eye_enc);
+
+        pos_left.resize(3);
+        pos_left[0] = left_eye_pose[0];
+        pos_left[1] = left_eye_pose[1];
+        pos_left[2] = left_eye_pose[2];
+
+        att_left.resize(4);
+        att_left[0] = left_eye_pose[3];
+        att_left[1] = left_eye_pose[4];
+        att_left[2] = left_eye_pose[5];
+        att_left[3] = left_eye_pose[6];
+
+        // Right eye dof from version and vergence
+        root_eye_enc(7) = bottle_head->get(4).asDouble() - bottle_head->get(5).asDouble() / 2.0;
+        Vector right_eye_pose = icub_kin_eye_right_.EndEffPose(M_PI / 180.0 * root_eye_enc);
+
+        // Extract pose
+        pos_right.resize(3);
+        pos_right[0] = right_eye_pose[0];
+        pos_right[1] = right_eye_pose[1];
+        pos_right[2] = right_eye_pose[2];
+
+        att_right.resize(4);
+        att_right[0] = right_eye_pose[3];
+        att_right[1] = right_eye_pose[4];
+        att_right[2] = right_eye_pose[5];
+        att_right[3] = right_eye_pose[6];
+
+        return true;
+    }
 }
 
 
@@ -86,19 +161,44 @@ bool GazeController::getCameraIntrinsics
     double &cy
 )
 {
-    Bottle info;
-    igaze->getInfo(info);
-    std::string key = "camera_intrinsics_" + eye_name;
-
-    if (info.find(key).isNull())
+    if ((eye_name != "left") && (eye_name != "right"))
         return false;
 
-    Bottle *list = info.find("camera_intrinsics_" + eye_name).asList();
+    if (igaze != nullptr)
+    {
+        Bottle info;
+        igaze->getInfo(info);
+        std::string key = "camera_intrinsics_" + eye_name;
 
-    fx = list->get(0).asDouble();
-    cx = list->get(2).asDouble();
-    fy = list->get(5).asDouble();
-    cy = list->get(6).asDouble();
+        if (info.find(key).isNull())
+            return false;
 
-    return true;
+        Bottle *list = info.find("camera_intrinsics_" + eye_name).asList();
+
+        fx = list->get(0).asDouble();
+        cx = list->get(2).asDouble();
+        fy = list->get(5).asDouble();
+        cy = list->get(6).asDouble();
+
+        return true;
+    }
+    else
+    {
+        if (eye_name == "left")
+        {
+            fx = fx_left_;
+            fy = fy_left_;
+            cx = cx_left_;
+            cy = cy_left_;
+        }
+        else if (eye_name == "right")
+        {
+            fx = fx_right_;
+            fy = fy_right_;
+            cx = cx_right_;
+            cy = cy_right_;
+        }
+
+        return true;
+    }
 }

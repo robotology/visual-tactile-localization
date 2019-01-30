8d725035959eafd950731d0ea488c500caa221a9
Nicola Piga <nicolapiga@gmail.com>
2019-01-30 13:59:53 +0100

    Re-implement class DiscretizedKinematicModel

--- src/object-tracking/src/DiscretizedKinematicModel.cpp
+++ src/object-tracking/src/DiscretizedKinematicModel.cpp
@@ -20,38 +20,28 @@ DiscretizedKinematicModel::DiscretizedKinematicModel
     F_.block<3, 3>(6, 6) = Matrix3d::Identity();
     F_.block<3, 3>(9, 6) = T * Matrix3d::Identity();
     F_.block<3, 3>(9, 9) = Matrix3d::Identity();
-    // F_ = MatrixXd::Identity(12, 12);
 
     Vector3d sigmas;
 
     // Compose noise covariance matrix for the linear acceleration part
     sigmas << sigma_x, sigma_y, sigma_z;
-    sigmas = sigmas.array().square();
     MatrixXd Q_pos(6, 6);
     Q_pos.block<3, 3>(0, 0) = sigmas.asDiagonal() * (std::pow(T, 3.0) / 3.0);
     Q_pos.block<3, 3>(0, 3) = sigmas.asDiagonal() * (std::pow(T, 2.0) / 2.0);
-    Q_pos.block<3, 3>(3, 0) = Q_pos.block<3, 3>(0, 3);
+    Q_pos.block<3, 3>(3, 0) = sigmas.asDiagonal() * (std::pow(T, 2.0) / 2.0);
     Q_pos.block<3, 3>(3, 3) = sigmas.asDiagonal() * T;
 
     // Compose noise covariance matrix for the euler angle rates part
     sigmas << sigma_yaw, sigma_pitch, sigma_roll;
-    sigmas = sigmas.array().square();
     MatrixXd Q_ang(6, 6);
-    Q_ang.block<3, 3>(3, 3) = sigmas.asDiagonal() * (std::pow(T, 3.0) / 3.0);
-    Q_ang.block<3, 3>(3, 0) = sigmas.asDiagonal() * (std::pow(T, 2.0) / 2.0);
-    Q_ang.block<3, 3>(0, 3) = Q_ang.block<3, 3>(3, 0);
     Q_ang.block<3, 3>(0, 0) = sigmas.asDiagonal() * T;
+    Q_ang.block<3, 3>(0, 3) = sigmas.asDiagonal() * (std::pow(T, 2.0) / 2.0);
+    Q_ang.block<3, 3>(3, 0) = sigmas.asDiagonal() * (std::pow(T, 2.0) / 2.0);
+    Q_ang.block<3, 3>(3, 3) = sigmas.asDiagonal() * (std::pow(T, 3.0) / 3.0);
 
     Q_ = MatrixXd::Zero(12, 12);
     Q_.block<6, 6>(0, 0) = Q_pos;
     Q_.block<6, 6>(6, 6) = Q_ang;
-
-    // Q_(0, 0) = sigma_x;
-    // Q_(1, 1) = sigma_y;
-    // Q_(2, 2) = sigma_z;
-    // Q_(9, 9) = sigma_yaw;
-    // Q_(10, 10) = sigma_pitch;
-    // Q_(11, 11) = sigma_roll;
 }
 
 

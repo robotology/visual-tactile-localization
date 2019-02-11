92a34e8e2d143fdf6ce5adc3171e7942d8a3b759
Nicola Piga <nicolapiga@gmail.com>
2019-02-11 19:01:40 +0100

    [WIP] Partial implementation of class iCubFingersEncoders

---
+++ src/icub-fingers-encoders/include/iCubFingersEncoders.h
@@ -0,0 +1,48 @@
+#ifndef ICUBFINGERSENCODERS_H
+#define ICUBFINGERSENCODERS_H
+
+#include <yarp/dev/IAnalogSensor.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/sig/Matrix.h>
+#include <yarp/os/ResourceFinder.h>
+
+#include <string>
+
+class iCubFingersEncoders
+{
+public:
+    iCubFingersEncoders(const std::string context, const std::string laterality, const std::string port_prefix);
+
+    virtual ~iCubFingersEncoders();
+
+protected:
+    std::pair<bool, yarp::sig::Vector> loadVectorDouble(const yarp::os::ResourceFinder& rf, const std::string key, const std::size_t size);
+
+    /**
+     * Indicates whether the PolyDriver interface is available.
+     * If not available, the encoder port is used instead.
+     */
+    bool use_interface_;
+
+    /**
+     * To be used if the driver is available, e.g. on the real robot.
+     */
+    yarp::dev::PolyDriver drv_analog_;
+
+    yarp::dev::IAnalogSensor *ianalog_;
+
+    /**
+     * To be used if the driver is not available, e.g. using dumped data.
+     */
+    yarp::os::BufferedPort<yarp::os::Bottle> port_analogs_;
+
+    /**
+     * Optional analog bounds.
+     */
+    yarp::sig::Matrix analog_bounds_;
+    bool use_bounds_;
+
+    const std::string log_ID_ = "ICUBFINGERSENCODERS";
+};
+
+#endif /* ICUBFINGERSENCODERS_H */
---
+++ src/icub-fingers-encoders/src/iCubFingersEncoders.cpp
@@ -0,0 +1,123 @@
+#include <iCubFingersEncoders.h>
+
+#include <yarp/os/Property.h>
+#include <yarp/os/ResourceFinder.h>
+#include <yarp/sig/Vector.h>
+
+#include <iostream>
+
+using namespace yarp::os;
+using namespace yarp::sig;
+
+
+iCubFingersEncoders::iCubFingersEncoders
+(
+    const std::string context,
+    const std::string laterality,
+    const std::string port_prefix
+) :
+    use_interface_(false)
+{
+    // Load configuration from config file
+    ResourceFinder rf;
+    rf.setVerbose(true);
+    rf.setDefaultConfigFile("analogs_configuration.ini");
+    rf.setDefaultContext(context);
+
+    // Get inner resource finder according to requested laterality
+    if ((laterality != "right") && (laterality != "left"))
+    {
+        throw std::runtime_error("ERROR::" + log_ID_ + "::CTOR::\nERROR: invalid laterality" + laterality + ".");
+    }
+    ResourceFinder inner_rf = rf.findNestedResourceFinder(laterality.c_str());
+    bool use_bounds_ = inner_rf.check("use_bounds", Value(false)).asBool();
+    if (use_bounds_)
+    {
+        bool valid_vector;
+        Vector bounds_col_0;
+        std::tie(valid_vector, bounds_col_0) = loadVectorDouble(inner_rf, "bounds_col_0", 16);
+        if (!valid_vector)
+        {
+            throw std::runtime_error("ERROR::" + log_ID_ + "::CTOR::\nERROR: bounds requested but not availalbe in the configuration file.");
+        }
+
+        Vector bounds_col_1;
+        std::tie(valid_vector, bounds_col_1) = loadVectorDouble(inner_rf, "bounds_col_1", 16);
+        if (!valid_vector)
+        {
+            throw std::runtime_error("ERROR::" + log_ID_ + "::CTOR::\nERROR: bounds requested but not availalbe in the configuration file.");
+        }
+
+        analog_bounds_.resize(16, 2);
+        analog_bounds_.setCol(0, bounds_col_0);
+        analog_bounds_.setCol(1, bounds_col_1);
+    }
+
+    // Try to use the PolyDriver interface
+    Property prop_analog;
+    prop_analog.put("device", "analogsensorclient");
+    prop_analog.put("local", "/" + port_prefix + "/" + laterality + "_hand/analog:i");
+    prop_analog.put("remote", "/icub/" + laterality + "_hand/analog:o");
+    if (drv_analog_.open(prop_analog))
+    {
+        // Try to retrieve the view
+        if (drv_analog_.view(ianalog_) && ianalog_ != nullptr)
+            use_interface_ = true;
+    }
+
+    if (!use_interface_)
+    {
+        std::cout << "INFO::" + log_ID_ + "::CTOR::\nINFO: PolyDriver interface IAnalogSensors not available. Using raw encoders from a port." << std::endl;
+
+        // If the PolyDriver is not available, use a standard port
+        if (!port_analogs_.open("/" + port_prefix + "/" + laterality + "_hand/analog:i"))
+        {
+            throw std::runtime_error("ERROR::" + log_ID_ + "::CTOR::\nERROR: unable to open port for analog encoders.");
+        }
+    }
+}
+
+
+iCubFingersEncoders::~iCubFingersEncoders()
+{
+
+}
+
+
+std::pair<bool, yarp::sig::Vector> iCubFingersEncoders::loadVectorDouble
+(
+    const ResourceFinder& rf,
+    const std::string key,
+    const std::size_t size
+    )
+{
+    bool ok = true;
+
+    if (rf.find(key).isNull())
+        ok = false;
+
+    Bottle* b = rf.find(key).asList();
+    if (b == nullptr)
+        ok = false;
+
+    if (b->size() != size)
+        ok = false;
+
+    if (!ok)
+        return std::make_pair(false, Vector());
+
+    Vector vector(size);
+    for (std::size_t i = 0; i < b->size(); i++)
+    {
+        Value item_v = b->get(i);
+        if (item_v.isNull())
+            return std::make_pair(false, Vector());
+
+        if (!item_v.isDouble())
+            return std::make_pair(false, Vector());
+
+        vector(i) = item_v.asDouble();
+    }
+
+    return std::make_pair(true, vector);
+}

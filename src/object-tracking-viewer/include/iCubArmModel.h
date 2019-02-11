a6afc912a771a792f50f8a87a47a37ed91076534
Nicola Piga <nicolapiga@gmail.com>
2019-02-11 17:53:37 +0100

    Enable use of hall encoders mounted on iCub fingertips in class iCubArmModel within module object-tracking-viewer

--- src/object-tracking-viewer/include/iCubArmModel.h
+++ src/object-tracking-viewer/include/iCubArmModel.h
@@ -7,6 +7,8 @@
 
 #include <SuperimposeMesh/SICAD.h>
 
+#include <yarp/dev/IAnalogSensor.h>
+#include <yarp/dev/PolyDriver.h>
 #include <yarp/os/Bottle.h>
 #include <yarp/os/BufferedPort.h>
 #include <yarp/os/ConstString.h>
@@ -40,6 +42,8 @@ protected:
     bool setArmJoints(const yarp::sig::Vector& q);
 
 private:
+    void setupAnalogBounds();
+
     const std::string log_ID_ = "[iCubArmModel]";
 
     const std::string port_prefix_ = "iCubArmModel";
@@ -63,6 +67,14 @@ private:
     yarp::os::BufferedPort<yarp::os::Bottle> port_torso_enc_;
 
     yarp::os::BufferedPort<yarp::os::Bottle> port_arm_enc_;
+
+    yarp::dev::PolyDriver drv_analog_;
+
+    yarp::dev::IAnalogSensor *ianalog_;
+
+    // matrix of analog bounds
+    // for encoders of proximal/distal joints
+    yarp::sig::Matrix analog_bounds_;
 };
 
 #endif /* ICUBARMMODEL_H */

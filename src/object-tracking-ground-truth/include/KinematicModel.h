66ac0629bab68a350de88000400aad306063950e
Nicola Piga <nicolapiga@gmail.com>
2019-02-21 17:43:36 +0100

    Implement class KinematicModel within module object-tracking-ground-truth

---
+++ src/object-tracking-ground-truth/include/KinematicModel.h
@@ -0,0 +1,60 @@
+#ifndef KINEMATICMODEL_H
+#define KINEMATICMODEL_H
+
+#include <BayesFilters/LinearStateModel.h>
+
+#include <Eigen/Dense>
+
+#include <chrono>
+
+class KinematicModel : public bfl::LinearStateModel
+{
+public:
+    KinematicModel
+    (
+        const double sigma_x,
+        const double sigma_y,
+        const double sigma_z,
+        const double sigma_phi,
+        const double sigma_theta,
+        const double sigma_psi
+    );
+
+    virtual ~KinematicModel();
+
+    Eigen::MatrixXd getStateTransitionMatrix() override;
+
+    bool setProperty(const std::string& property) override;
+
+    Eigen::MatrixXd getNoiseCovarianceMatrix();
+
+    std::pair<std::size_t, std::size_t> getOutputSize() const override;
+
+protected:
+    void evaluateStateTransitionMatrix(const double T);
+
+    void evaluateNoiseCovarianceMatrix(const double T);
+
+    /**
+     * State transition matrix.
+     */
+    Eigen::MatrixXd F_;
+
+    /**
+     * Noise covariance matrix.
+     */
+    Eigen::MatrixXd Q_;
+
+    /**
+     * Squared power spectral densities
+     */
+    Eigen::VectorXd sigma_position_;
+
+    Eigen::VectorXd sigma_orientation_;
+
+    std::chrono::high_resolution_clock::time_point last_time_;
+
+    bool last_time_set_ = false;
+};
+
+#endif /* KINEMATICMODEL_H */

aaa28089b7f697fc3c7af3281970335cc2630d03
Nicola Piga <nicolapiga@gmail.com>
2019-02-24 16:40:32 +0100

    Half the number of point sampled on the fingertips in order to reduce the computational load in ctor of class iCubHandContactsModel

--- src/object-tracking/include/iCubHandContactsModel.h
+++ src/object-tracking/include/iCubHandContactsModel.h
@@ -59,7 +59,7 @@ protected:
 
     const std::string log_ID_ = "[ICUBHANDCONTACTSMODEL]";
 
-    const std::size_t fingertip_number_samples_ = 50;
+    const std::size_t fingertip_number_samples_ = 25;
 };
 
 #endif /* ICUBHANDCONTACTSMODEL_H */

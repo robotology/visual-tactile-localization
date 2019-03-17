af2d39556a002fc007d6dd444339c4726a7d6dcd
Nicola Piga <nicolapiga@gmail.com>
2019-03-17 14:35:09 +0100

    Do not call method BoundingBoxEstimator::enableObjectFeedforward in method PFilter::filteringStep

--- src/object-tracking/src/PFilter.cpp
+++ src/object-tracking/src/PFilter.cpp
@@ -288,8 +288,6 @@ void PFilter::filteringStep()
 
     // Use estimate as hint for the bounding box estimator
     bbox_estimator_->setObjectPose(cor_particle_.mean(), cor_particle_.weight());
-    // Enable/disable bounding box object feedforward term according to the current state of occlusion
-    // bbox_estimator_->enableObjectFeedforward(!(icub_point_cloud_share_->getOcclusion()));
     // Eanble/disable bounding box hand feedforward term according to the corrent state of contact
     bbox_estimator_->enableHandFeedforward(icub_point_cloud_share_->getContactState());
 

277adcf92270e4a1e9271e10302a8ce8c120b01c
Nicola Piga <nicolapiga@gmail.com>
2019-04-29 01:20:22 +0200

    Implement abstract class ObjectSampler
    Abstract a object surface sampler to be used within point cloud prediction classes

---
+++ src/object-tracking/src/ObjectSampler.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <ObjectSampler.h>
+
+
+ObjectSampler::~ObjectSampler()
+{ }

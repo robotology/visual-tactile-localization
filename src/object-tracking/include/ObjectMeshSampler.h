0cc99e59aab72a226431a85711ca27dff4a1d3a8
Nicola Piga <nicolapiga@gmail.com>
2019-04-29 01:22:23 +0200

    Implement class ObjectMeshSampling inheriting from ObjectSampling
    Implement object surface sampling for objects represented using triangle meshes

---
+++ src/object-tracking/include/ObjectMeshSampler.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef OBJECTMESHSAMPLER_H
+#define OBJECTMESHSAMPLER_H
+
+#include <MeshImporter.h>
+#include <ObjectSampler.h>
+#include <VCGTriMesh.h>
+
+#include <string>
+
+class ObjectMeshSampler : public ObjectSampler, MeshImporter
+{
+public:
+    ObjectMeshSampler(const std::string& mesh_filename);
+
+    virtual ~ObjectMeshSampler();
+
+    std::pair<bool, Eigen::MatrixXd> sample(const std::size_t& number_of_points) override;
+
+protected:
+    simpleTriMesh trimesh_;
+
+    const std::string log_ID_ = "ObjectMeshSampler";
+};
+
+#endif

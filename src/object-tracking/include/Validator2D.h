c4eeab5931b07e00cdf44fe8d195346b788af3e8
Nicola Piga <nicolapiga@gmail.com>
2019-06-09 15:20:24 +0200

    Add class Validator2D

---
+++ src/object-tracking/include/Validator2D.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef VALIDATOR2D_H
+#define VALIDATOR2D_H
+
+#include <Eigen/Dense>
+
+#include <Camera.h>
+#include <PointCloudPrediction.h>
+
+#include <memory>
+
+#include <yarp/os/BufferedPort.h>
+#include <yarp/sig/Image.h>
+
+
+class Validator2D
+{
+public:
+    Validator2D(std::shared_ptr<PointCloudPrediction> point_cloud_prediction, const std::string& port_prefix, const std::string& camera_name, const std::string& camera_fallback_key, const std::string& camera_laterality = "");
+
+    ~Validator2D();
+
+    bool renderEvaluation(const Eigen::Transform<double, 3, Eigen::Affine> object_pose);
+
+private:
+    std::unique_ptr<Camera> camera_;
+
+    std::shared_ptr<PointCloudPrediction> prediction_;
+
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb>> port_image_in_;
+
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb>> port_image_out_;
+
+    const std::string log_ID_ = "Validator2D";
+};
+
+#endif /* VALIDATOR2D_H */
---
+++ src/object-tracking/src/Validator2D.cpp
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+#include <Validator2D.h>
+#include <CameraParameters.h>
+#include <iCubCamera.h>
+#include <RealsenseCamera.h>
+
+#include <yarp/cv/Cv.h>
+#include <yarp/sig/Image.h>
+
+
+using namespace Eigen;
+using namespace yarp::cv;
+using namespace yarp::sig;
+
+
+Validator2D::Validator2D
+(
+    std::shared_ptr<PointCloudPrediction> point_cloud_prediction,
+    const std::string& port_prefix,
+    const std::string& camera_name,
+    const std::string& camera_fallback_key,
+    const std::string& camera_laterality
+) :
+    prediction_(point_cloud_prediction)
+{
+    if (!port_image_in_.open("/" + port_prefix + "/validationImage:i"))
+    {
+        std::string err = log_ID_ + "::ctor. Error: cannot open background input port.";
+        throw(std::runtime_error(err));
+    }
+
+    if (!port_image_out_.open("/" + port_prefix + "/validationImage:o"))
+    {
+        std::string err = log_ID_ + "::ctor. Error: cannot open output image port.";
+        throw(std::runtime_error(err));
+    }
+
+    // Initialize camera required to extract depth
+    if (camera_name == "iCubCamera")
+    {
+        camera_ = std::unique_ptr<iCubCamera>(new iCubCamera(camera_laterality, port_prefix, "object-tracking", camera_fallback_key));
+    }
+    else if (camera_name == "RealsenseCamera")
+    {
+        camera_ = std::unique_ptr<RealsenseCamera>(new RealsenseCamera(port_prefix, "object-tracking", camera_fallback_key));
+    }
+    else
+    {
+        std::string err = log_ID_ + "::ctor. The requested camera is not available. Requested camera is " + camera_name;
+        throw(std::runtime_error(err));
+    }
+    camera_->initialize();
+}
+
+
+Validator2D::~Validator2D()
+{
+    port_image_in_.close();
+    port_image_out_.close();
+}
+
+
+bool Validator2D::renderEvaluation(const Transform<double, 3, Affine> object_pose)
+{
+    // Take input image
+    ImageOf<PixelRgb>* image_in_yarp;
+    image_in_yarp = port_image_in_.read(false);
+
+    if (image_in_yarp == nullptr)
+        return false;
+
+    // Convert to cv::Mat
+    cv::Mat image_in = yarp::cv::toCvMat(*image_in_yarp);
+    cv::cvtColor(image_in, image_in, cv::COLOR_RGB2BGR);
+
+    // Express pose with respect to the camera reference frame
+    bool valid_pose = false;
+    Transform<double, 3, Affine> camera_pose;
+
+    std::tie(valid_pose, camera_pose) = camera_->getCameraPose(false);
+    if (!valid_pose)
+        return false;
+
+    Transform<double, 3, Affine> cam_to_object = camera_pose.inverse() * object_pose;
+
+    // Get the model virtual point cloud from class PointCloudPrediction
+    MatrixXd cloud = prediction_->evaluateModel(cam_to_object);
+
+    // Get camera intrinsic parameters
+    bool valid_camera_parameters;
+    CameraParameters camera_parameters;
+    std::tie(valid_camera_parameters, camera_parameters) = camera_->getIntrinsicParameters();
+    if (!valid_camera_parameters)
+        return false;
+
+    // Transform to a cv contour, i.e. a std::vector of std::vector of cv::Point
+    // std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Point> contour(cloud.cols());
+
+#pragma omp parallel for
+    for (std::size_t i = 0; i < cloud.cols(); i++)
+    {
+        int u = camera_parameters.cx + cloud.col(i)(0) / cloud.col(i)(2) * camera_parameters.fx;
+        int v = camera_parameters.cy + cloud.col(i)(1) / cloud.col(i)(2) * camera_parameters.fy;
+
+        contour.at(i) = cv::Point(u, v);
+    }
+
+    // contours.push_back(contour);
+
+    // Evaluate convex hull
+    std::vector<std::vector<cv::Point>> hull(1);
+    cv::convexHull(contour, hull[0]);
+
+    // Superimpose on input image
+    cv::drawContours(image_in, hull, 0, cv::Scalar(255, 0, 0), 3);
+
+    // Send validation image
+    cv::cvtColor(image_in, image_in, cv::COLOR_BGR2RGB);
+    ImageOf<PixelRgb>& image_out = port_image_out_.prepare();
+    image_out = fromCvMat<PixelRgb>(image_in);
+    port_image_out_.write();
+
+    return true;
+}

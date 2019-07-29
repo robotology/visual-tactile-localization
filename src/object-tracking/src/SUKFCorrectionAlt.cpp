9cc9914404e5854cb7d19ce8fb1612d3f59c3f38
Nicola Piga <nicolapiga@gmail.com>
2019-07-29 10:46:23 +0200

    Implement correction using 3d measurements and 2d mask measurements in method SUKFCorrection::correctStep()

--- src/object-tracking/src/SUKFCorrectionAlt.cpp
+++ src/object-tracking/src/SUKFCorrectionAlt.cpp
@@ -9,6 +9,10 @@
 #include <BayesFilters/directional_statistics.h>
 #include <BayesFilters/utils.h>
 
+#include <opencv2/opencv.hpp>
+
+#include <unordered_map>
+
 using namespace bfl;
 using namespace bfl::directional_statistics;
 using namespace bfl::sigma_point;
@@ -92,7 +96,115 @@ void SUKFCorrection::correctStep(const GaussianMixture& pred_state, GaussianMixt
     }
 
     /* Cast data to MatrixXd. */
-    propagated_sigma_points_ = bfl::any::any_cast<MatrixXd&&>(std::move(pred));
+    std::tuple<std::size_t, MatrixXd, std::vector<MatrixXd>> prediction_tuple = bfl::any::any_cast<std::tuple<std::size_t, MatrixXd, std::vector<MatrixXd>>>(pred);
+    // propagated_sigma_points_ = bfl::any::any_cast<MatrixXd&&>(std::move(pred));
+    std::size_t data_size_3d = std::get<0>(prediction_tuple);
+    propagated_sigma_points_ = std::get<1>(prediction_tuple);
+    std::vector<MatrixXd> segmentation_predictions = std::get<2>(prediction_tuple);
+
+
+    /* Get the current measurement if available. */
+    bool valid_measurement;
+    Data measurement;
+    std::tie(valid_measurement, measurement) = measurement_model_->measure();
+    MatrixXd measurement_eigen = bfl::any::any_cast<MatrixXd&&>(std::move(measurement));
+
+    /* Remove useless points from segmentation predictions. */
+    double delta = 0.2;
+    Ref<MatrixXd> segmentation_meas = measurement_eigen.bottomRows(measurement_eigen.rows() - data_size_3d * 3);
+    // VectorXd segmentation_meas_scaled(segmentation_meas.rows(), 1);
+    std::vector<std::vector<cv::Point>> contours(1);
+    VectorXd mean = VectorXd::Zero(2);
+    for (std::size_t i = 0; i < segmentation_meas.size() / 2; i++)
+        mean += segmentation_meas.col(0).segment(i * 2, 2);
+    mean /= (segmentation_meas.size() / 2 );
+    for (std::size_t i = 0; i < segmentation_meas.size() / 2; i++)
+    {
+        VectorXd scaled_point = segmentation_meas.col(0).segment(i * 2, 2) + (segmentation_meas.col(0).segment(i * 2, 2) - mean) * delta;
+        contours[0].push_back(cv::Point(scaled_point(0), scaled_point(1)));
+    }
+    for (std::size_t i = 0; i < segmentation_predictions.size(); i++)
+    {
+        Ref<MatrixXd> segmentation = segmentation_predictions.at(i);
+        VectorXi valids = VectorXi::Zero(segmentation.cols());
+        for (std::size_t j = 0; j < segmentation.cols(); j++)
+        {
+            if (cv::pointPolygonTest(contours[0], cv::Point2f(segmentation.col(j)(0), segmentation.col(j)(1)), false) >= 0)
+                valids(j) = 1;
+        }
+        MatrixXd cleaned_segmentation(2, valids.sum());
+        // std::cout << "i: " << i << std::endl;
+        // std::cout << "size:" << valids.sum() << std::endl;
+        for (std::size_t j = 0, k = 0; j < segmentation.cols(); j++)
+            if (valids(j) == 1)
+            {
+                cleaned_segmentation.col(k) = segmentation.col(j);
+                k++;
+            }
+
+        segmentation_predictions.at(i) = cleaned_segmentation;
+    }
+
+    /* Remove useless mask measurements. */
+    Map<MatrixXd> segmentation_meas_cols(segmentation_meas.data(), 2, segmentation_meas.size() / 2);
+    VectorXi valid_measure = VectorXi::Zero(segmentation_meas_cols.cols());
+    for (std::size_t i = 0; i < segmentation_predictions.size(); i++)
+    {
+        Ref<MatrixXd> segmentation = segmentation_predictions.at(i);
+
+        for (std::size_t j = 0; j < segmentation.cols(); j++)
+        {
+            std::size_t min_index;
+            (segmentation_meas_cols.colwise() - segmentation.col(j)).colwise().norm().minCoeff(&min_index);
+            valid_measure(min_index) = 1;
+        }
+    }
+
+    /* Store valid measurements. */
+    VectorXd cleaned_meas(valid_measure.sum() * 2);
+    for (std::size_t i = 0, j = 0; i < valid_measure.size(); i++)
+    {
+        if (valid_measure(i) == 1)
+        {
+            cleaned_meas.segment(j * 2, 2) = segmentation_meas_cols.col(i);
+            j++;
+        }
+    }
+
+    /* Store valid associations. */
+    MatrixXd cleaned_predictions(cleaned_meas.size(), (pred_state.dim * 2) + 1);
+    for (std::size_t i = 0; i < cleaned_predictions.cols(); i++)
+    {
+        if (segmentation_predictions.at(i).cols() == 0)
+        {
+            cleaned_predictions.col(i) = cleaned_meas;
+        }
+        else
+        {
+            for (std::size_t j = 0; j < cleaned_meas.size() / 2; j++)
+            {
+                std::size_t min_index;
+                (segmentation_predictions.at(i).colwise() - cleaned_meas.segment(j * 2, 2)).colwise().norm().minCoeff(&min_index);
+                cleaned_predictions.col(i).segment(j * 2, 2) = segmentation_predictions.at(i).col(min_index);
+            }
+        }
+    }
+
+    // std::cout << "*" << std::endl;
+    // std::cout << cleaned_meas.transpose() << std::endl;
+    // std::cout << cleaned_predictions.transpose() << std::endl;
+
+    std::size_t data_size_2d = cleaned_meas.size() / 2;
+    VectorXd point_cloud = measurement_eigen.col(0).head(data_size_3d * 3);
+
+    measurement_eigen.resize(data_size_3d * 3 + data_size_2d * 2, 1);
+    measurement_eigen.col(0).head(data_size_3d * 3) = point_cloud;
+    measurement_eigen.col(0).tail(data_size_2d * 2) = cleaned_meas;
+
+    MatrixXd propagated_sigmas = propagated_sigma_points_;
+    propagated_sigma_points_.resize(data_size_3d * 3 + data_size_2d * 2, NoChange);
+    propagated_sigma_points_.topRows(data_size_3d  * 3) = propagated_sigmas;
+    propagated_sigma_points_.bottomRows(data_size_2d * 2).swap(cleaned_predictions);
 
     /* Evaluate the predicted mean. */
     std::size_t size_sigmas = (pred_state.dim * 2) + 1;
@@ -105,26 +217,23 @@ void SUKFCorrection::correctStep(const GaussianMixture& pred_state, GaussianMixt
         pred_mean.col(i).noalias() = prop_sp * ut_weight_.mean;
     }
 
-    /* Get the current measurement if available. */
-    bool valid_measurement;
-    Data measurement;
-    std::tie(valid_measurement, measurement) = measurement_model_->measure();
-
     /* Check if the size of the measurement is compatible with measurement_sub_size. */
-    std::pair<std::size_t, std::size_t> sizes = measurement_model_->getOutputSize();
-    std::size_t meas_size = sizes.first + sizes.second;
-    valid_measurement &= (((meas_size) % measurement_sub_size_ ) == 0);
+    // std::pair<std::size_t, std::size_t> sizes = measurement_model_->getOutputSize();
+    // std::size_t meas_size = sizes.first + sizes.second;
+    // valid_measurement &= (((meas_size) % measurement_sub_size_ ) == 0);
 
-    if (!valid_measurement)
-    {
-        corr_state = pred_state;
-        return;
-    }
+    // if (!valid_measurement)
+    // {
+    //     corr_state = pred_state;
+    //     return;
+    // }
 
     /* Evaluate the innovation if possible. */
     bool valid_innovation;
     Data innovation;
-    std::tie(valid_innovation, innovation) = measurement_model_->innovation(pred_mean, measurement);
+    // MatrixXd measurement_3d;
+    // measurement_3d = measurement_eigen.col(0).head(data_size_3d * 3);
+    std::tie(valid_innovation, innovation) = measurement_model_->innovation(pred_mean, measurement_eigen);
 
     if (!valid_innovation)
     {
@@ -160,7 +269,8 @@ void SUKFCorrection::correctStep(const GaussianMixture& pred_state, GaussianMixt
         MatrixXd C_inv = MatrixXd::Identity(size_sigmas, size_sigmas);
         VectorXd d = VectorXd::Zero(size_sigmas);
 
-        for (size_t j = 0; j < (meas_size) / measurement_sub_size_; j++)
+        std::size_t j;
+        for (j = 0; j < data_size_3d; j++)
         {
             MatrixXd tmp(size_sigmas, measurement_sub_size_);
             tmp.noalias() = Y.middleRows(measurement_sub_size_ *j, measurement_sub_size_).transpose() * getNoiseCovarianceMatrix(j).inverse();
@@ -168,6 +278,15 @@ void SUKFCorrection::correctStep(const GaussianMixture& pred_state, GaussianMixt
             C_inv += tmp * Y.middleRows(measurement_sub_size_ *j, measurement_sub_size_);
 
             d += tmp * innovations_.col(i).middleRows(measurement_sub_size_ *j, measurement_sub_size_);
+        }
+        for (std::size_t k = 0; k < data_size_2d; k++)
+        {
+            MatrixXd tmp(size_sigmas, measurement_sub_size_);
+            tmp.noalias() = Y.middleRows(j * measurement_sub_size_  + 2 * k, 2).transpose() * getNoiseCovarianceMatrix(point_cloud.size() / measurement_sub_size_ + k).inverse();
+
+            C_inv += tmp * Y.middleRows(j * measurement_sub_size_  + 2 * k, 2);
+
+            d += tmp * innovations_.col(i).middleRows(j * measurement_sub_size_  + 2 * k, 2);
         }
 
         /* Process input sigma points.
